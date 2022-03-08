d6fe03aacff1dcca1626fb64f13f17cba05271dc
NokASur <75212119+NokASur@users.noreply.github.com>
2022-03-08 17:18:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import pygame, time
 from player import *
 from colours import *
-
+from characteristics import *
 
 fpsClock = pygame.time.Clock()
 pygame.init()
-FPS = 60
-W = 800
-H = 800
-PG_Width = 20
-Block_Width = 18
 window_size = (W, H)
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("2Dtyan")
 inGame = True
 
 all_sprites = pygame.sprite.Group()
+
 new_rect = Player()
+
 all_sprites.add(new_rect)
 
-r = [0, 0, Block_Width, Block_Width]
 
 matrix = []
 for y in range(int(W / PG_Width)):
@@ -34,10 +30,11 @@ while inGame:
         if event.type == pygame.QUIT:
             inGame = False
 
+    screen.fill(BLACK)
+
     for y in range(int(W / PG_Width)):
         for x in range(int(W / PG_Width)):
-            r[0], r[1] = PG_Width * y + 1, PG_Width * x + 1
-            pygame.draw.rect(screen, YELLOW, r, 0)
+            pygame.draw.rect(screen, YELLOW, (PG_Width * y + 1, PG_Width * x + 1, Block_Width, Block_Width), 0)
 
     all_sprites.update()
     all_sprites.draw(screen)
