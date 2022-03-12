924ba4227237b223ffa5ca1cec0bc1f0b13b072f
ArtemSurovSergeevich <75212119+NokASur@users.noreply.github.com>
2022-03-12 19:31:50 +0300

    Update characteristics.py

--- characteristics.py
+++ characteristics.py
@@ -1,5 +1,14 @@
+# window and grid options
+
 FPS = 60
 W = 800
 H = 800
 PG_Width = 20
 Block_Width = 18
+
+# enemy stats
+enemy_stat = {"assault_troop": {"health": 4, "speed": 1.5, "intelligence": "straight_up_engagement", "w": 15, "h": 15},
+              "artillery": {"health": 5, "speed": 1, "intelligence": "distancing", "w": 18, "h": 18},
+              "spawner": {"health": 7, "speed": 1, "intelligence": "avoiding", "w": 20, "h": 20}}
+
+
